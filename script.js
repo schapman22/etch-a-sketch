ce641a3809ad74c13b99dd271855088d367178cf
schapman22 <schapman22@gmail.com>
2023-06-11 21:26:43 -0400

    16x16 grid

---
+++ script.js
@@ -0,0 +1,17 @@
+function createGrid() {
+    const container = document.getElementById("container");
+
+    for (i = 1; i <= 16; i++) {
+        for ( j = 1; j <= 16; j++) {
+            const square = document.createElement("div");
+            square.classList.add("grid-square");
+            container.appendChild(square);
+        }
+    }
+}
+
+window.addEventListener("load", createGrid);
+
+
+
+
